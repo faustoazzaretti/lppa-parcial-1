f0c72a1fefe3cfa3bbab00b2670b1596630f9322
Fausto Azzaretti <faustoazzaretti95@gmail.com>
2020-05-23 10:42:25 -0300

    Add comments in javascript functions

--- js/app.js
+++ js/app.js
@@ -22,6 +22,8 @@ var elegirError = null
 var elegir = null;
 var pattern = /^[a-zA-Z0-9\-_]+(\.[a-zA-Z0-9\-_]+)*@[a-z0-9]+(\-[a-z0-9]+)*(\.[a-z0-9]+(\-[a-z0-9]+)*)*\.[a-z]{2,4}$/;
 
+/*Funcion para validar nombre, si el lenght del input es 1 o 2, te indica Nombre corto, si esta vacio te dice campo incompleto
+y si el nombre es correcto no muestra ningun error*/
 function validarNombre() {
     if (nombreUsuario.value.length < 3 && nombreUsuario.value.length > 0) {
         nombreUsuarioError.innerHTML = "Nombre demasiado corto."
@@ -32,6 +34,8 @@ function validarNombre() {
     }
 }
 
+/*Funcion para validar apellido, si el lenght del input es 1 o 2, te indica Nombre corto, si esta vacio te dice campo incompleto
+y si el nombre es correcto no muestra ningun error*/
 function validarApellido() {
     if (apellidoUsuario.value.length < 3 && apellidoUsuario.value.length > 0) {
         apellidoUsuarioError.innerHTML = "Apellido demasiado corto."
@@ -42,6 +46,7 @@ function validarApellido() {
     }
 }
 
+/*Funcion para validad edad, si no es un numero, o la edad es menor que 0 y mayor que 99 te indica el error*/
 function validarEdad() {
     if (isNaN(edad.value) || edad.value < 1 || edad.value > 99) {
         errorEdad.innerHTML = "Edad Invalida";
@@ -50,6 +55,8 @@ function validarEdad() {
     }
 }
 
+/*Funcion para validad email, hace un test, el test hace la comparacion entre mi expresion regular llamada pattern,
+y con el value del email que ingrese, si es correcto devuelve true de lo contrario muestra el error.*/
 function validateEmail() {
     if (pattern.test(email.value)) {
         emailError.innerHTML = ""
@@ -58,6 +65,7 @@ function validateEmail() {
     }
 }
 
+/*Funcion para sexo, comprueba si todos los radiobuttons estan sin checkear muestra error para que puedas seleccionar uno*/
 function validarSexo() {
     if (men.checked === false && women.checked === false && other.checked === false) {
         sexoError.innerHTML = "Debe seleccionar un sexo"
@@ -67,6 +75,7 @@ function validarSexo() {
     }
 }
 
+/*Funcion para validad interes, lo mismo que el de arriba, pero en esta ocasion son checkboxs*/
 function validarInteres() {
     if (musica.checked === false && deportes.checked === false && juegos.checked === false && tecnologia.checked === false) {
         textInteresError.innerHTML = "Debe seleccionar un interes"
@@ -75,6 +84,7 @@ function validarInteres() {
     }
 }
 
+/*Funcion para comentario, en este caso si el lenght es menor que 15 te indica el error para que lo completes*/
 function validarComentario() {
     if (comentarios.value.length < 15) {
         comentariosError.innerHTML = 'El comentario debe tener al menos 15 caracteres'
@@ -83,6 +93,7 @@ function validarComentario() {
     }
 }
 
+/*Funcion para mostrar los datos en la consola*/
 function mostrarDatos() {
     console.log('Nombre: ' + nombreUsuario.value)
     console.log('Apellido: ' + apellidoUsuario.value)
@@ -111,7 +122,7 @@ function mostrarDatos() {
     console.log('Comentarios: ' + comentarios.value)
 }
 
-
+/*Funcion para enviar formulario, se va a ejecutar cuando el usuario hace click en el boton*/
 var enviarFormulario = function () {
     console.clear()
     validarNombre()
